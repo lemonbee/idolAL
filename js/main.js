3f2c696ca17f0caee72dfcf7fac6e41c076c27ec
Alice <liuyushu1988@gmail.com>
2017-08-09 23:58:05 +0800

    Add second page with idol and refresh buttons

--- js/main.js
+++ js/main.js
@@ -11,6 +11,15 @@ $('.logSubmit-button').click(function(){
 	$(".page-logon").remove();
 	$(".page-select").show();
 })
+$('.femaleidol-btn').click(function(){
+	alert("女神");
+})
+$('.maleidol-btn').click(function(){
+	alert("男神");
+})
+$('.refresh-btn').click(function(){
+	alert("换一批");
+})
 //$(".edit-page").show();
 });
 
